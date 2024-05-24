239376ba3a14d308b94df9461ab9fffee1e98ee1
Hol1ow <110432668+Hol1ow@users.noreply.github.com>
2024-05-23 20:40:21 -0400

    initialize the project

---
+++ src/client/java/com/name/finerf5/FinerF5Client.java
@@ -0,0 +1,10 @@
+package com.name.finerf5;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FinerF5Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
