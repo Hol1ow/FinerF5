22219ccf7d0b99dec0256ff16637736ff985d88a
Hol1ow <110432668+Hol1ow@users.noreply.github.com>
2024-05-24 20:41:43 -0400

    finish the logic of toggle btn

--- src/client/java/com/pang/finerf5/FinerF5Client.java
+++ src/client/java/com/pang/finerf5/FinerF5Client.java
@@ -6,33 +6,45 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.Perspective;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 
 
 public class FinerF5Client implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		KeyBinding ff2tfToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-			"1st to 3rd Toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F5, "Finer F5"));
+		KeyBinding fp2tpbToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"Toggle between 1st & 3rd back", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F6, "Finer F5"));
 
-		KeyBinding ff2sfToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-			"1st to 2nd Toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F6, "Finer F5"));
+		KeyBinding fp2tpfToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"Toggle between 1st & 3rd front", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F7, "Finer F5"));
 
-		KeyBinding tf2sfToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-			"3rd to 2nd Toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F7, "Finer F5"));
+		KeyBinding tpb2tpfToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"Toggle between 3rd back & front", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "Finer F5"));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while(ff2tfToggle.wasPressed()) {
-				client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+			while(fp2tpbToggle.wasPressed()) {
+				if (client.options.getPerspective() != Perspective.FIRST_PERSON) {
+					client.options.setPerspective(Perspective.FIRST_PERSON);
+				} else {
+					client.options.setPerspective(Perspective.THIRD_PERSON_BACK);
+				}
 			}
 
-			while(ff2sfToggle.wasPressed()) {
-				client.player.sendMessage(Text.literal("Key 2 was pressed!"), false);
+			while(fp2tpfToggle.wasPressed()) {
+				if (client.options.getPerspective() != Perspective.FIRST_PERSON) {
+					client.options.setPerspective(Perspective.FIRST_PERSON);
+				} else {
+					client.options.setPerspective(Perspective.THIRD_PERSON_FRONT);
+				}
 			}
 
-			while(tf2sfToggle.wasPressed()) {
-				client.player.sendMessage(Text.literal("Key 3 was pressed!"), false);
+			while(tpb2tpfToggle.wasPressed()) {
+				if (client.options.getPerspective() != Perspective.THIRD_PERSON_BACK) {
+					client.options.setPerspective(Perspective.THIRD_PERSON_BACK);
+				} else {
+					client.options.setPerspective(Perspective.THIRD_PERSON_FRONT);
+				}
 			}
 		});
 	}
